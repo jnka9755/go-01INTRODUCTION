25ea38e412406033dca6e11df3c1c5eba1410ce2
Juan Santana <juanka9755@hotmail.com>
2023-04-24 10:41:16 -0500

    01-INTRODUCTION
    Funciones em GO
    mas de un retorno
    con sobrecarga estática y dinamia
    
    Retorno de errores
    
    Patrón Factory

--- functions/function/function.go
+++ functions/function/function.go
@@ -1,6 +1,87 @@
 package function
-package function
 
-func Add(x int , y int) int {
+import (
+	"errors"
+	"fmt"
+)
+
+type Operation int
+
+const (
+	SUM Operation = iota
+	SUB
+	DIV
+	MUL
+)
+
+func Add(x int, y int) int {
 	return x + y
+}
+
+func RepeatString(increment int, value string) {
+	for i := 0; i < increment; i++ {
+		fmt.Println(value)
+	}
+}
+
+func Calc(op Operation, x, y float64) (float64, error) {
+	switch op {
+	case SUM:
+		return x + y, nil
+	case SUB:
+		return x - y, nil
+	case DIV:
+		if y == 0 {
+			return 0, errors.New("No es posible dividir por 0")
+		}
+		return x / y, nil
+	case MUL:
+		return x * y, nil
+	default:
+		return 0, errors.New("Ha ocurrido un error")
+	}
+}
+
+func Split(value float32) (x, y float32) {
+	x = value * 4 / 9
+	y = value - x
+
+	return
+}
+
+func MSum(values ...float32) float32 {
+
+	var sum float32
+	for _, v := range values {
+		sum += v
+	}
+
+	return sum
+}
+
+func MOperations(op Operation, values ...float64) (float64, error) {
+	if len(values) == 0 {
+		return 0, errors.New("No hay valores")
+	}
+
+	calc := values[0]
+
+	for _, v := range values[1:] {
+		switch op {
+		case SUM:
+			calc += v
+		case SUB:
+			calc -= v
+		case DIV:
+			if v == 0 {
+				return 0, errors.New("No es posible dividir por 0")
+			}
+			calc /= v
+		case MUL:
+			calc *= v
+		default:
+			return 0, errors.New("Ha ocurrido un error")
+		}
+	}
+	return calc, nil
 }
--- functions/main.go
+++ functions/main.go
@@ -7,6 +7,68 @@ import (
 )
 
 func main() {
-	value := function.Add(3, 4)
+	fmt.Println(function.Add(3, 4))
+
+	function.RepeatString(5, "Juan")
+
+	fmt.Println()
+
+	value, error := function.Calc(function.SUM, 5, 3)
+
+	if error != nil {
+		fmt.Println("Error: ", error)
+	} else {
+		fmt.Println("Value: ", value)
+	}
+
+	value, error = function.Calc(function.DIV, 10, 0)
+
+	if error != nil {
+		fmt.Println("Error: ", error)
+	} else {
+		fmt.Println("Value: ", value)
+	}
+
+	fmt.Println()
+	x, y := function.Split(2)
+	fmt.Println(x, y)
+
+	fmt.Println()
+	value = float64(function.MSum(1, 2, 3, 4, 5, 6, 7, 8, 9))
 	fmt.Println(value)
+
+	fmt.Println()
+
+	value, error = function.MOperations(function.SUM, 1, 2, 3)
+	fmt.Println(value, error)
+
+	value, error = function.MOperations(function.SUB, 1, 2, 3)
+	fmt.Println(value, error)
+
+	value, error = function.MOperations(function.DIV, 1, 2, 3)
+	fmt.Println(value, error)
+
+	value, error = function.MOperations(function.MUL, 1, 2, 3)
+	fmt.Println(value, error)
+
+	fmt.Println()
+
+	fn := function.FactoryOperation(function.SUM)
+	value = fn(1, 2)
+
+	fmt.Println(value)
+
+	fn = function.FactoryOperation(function.SUB)
+	value = fn(1, 2)
+
+	fmt.Println(value)
+	fn = function.FactoryOperation(function.DIV)
+	value = fn(1, 2)
+
+	fmt.Println(value)
+	fn = function.FactoryOperation(function.MUL)
+	value = fn(1, 2)
+
+	fmt.Println(value)
+
 }
